396629021ba84f66746f2a7b5a2957feb741d8e6
Eren Gazioglu <eren.g94@gmail.com>
2026-01-22 01:01:40 +0100

    draw different things

--- NOTES.md
+++ NOTES.md
@@ -1,1 +1,9 @@
 # so_long | Notes
+
+## Images: MAGICK
+
+```bash
+for file in assets/*.bmp; 
+do magick "$file" "${file%.png}.xpm"; 
+done
+```
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 17:50:53 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/01/22 00:22:31 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/01/22 00:40:47 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	main(void)
 	void	*mlx;
 	void	*window;
 	void	*wall;
+	void	*empty;
+	void	*exit;
 	int		w;
 	int		h;
 	
@@ -28,7 +30,9 @@ int	main(void)
 		10 * GRID_SIZE * GRID_MULT, 
 		"Hello!"
 	);
-	wall = mlx_xpm_file_to_image(mlx, "assets/wall-01-x3.xpm", &w, &h);
+	wall = mlx_xpm_file_to_image(mlx, "assets/wall-01_x3.xpm", &w, &h);
+	empty = mlx_xpm_file_to_image(mlx, "assets/empty-01_x3.xpm", &w, &h);
+	exit = mlx_xpm_file_to_image(mlx, "assets/exit-01-x3.xpm", &w, &h);
 	if (!wall)
 	{
 		ft_printf("Malloc error, quitting\n");
@@ -36,11 +40,21 @@ int	main(void)
 	}
 	for (int x = 0; x < 10; x++)
 	{
-		for (int y = 0; y < 10; y++)
-		{
-			mlx_put_image_to_window(mlx, window, wall, w * x, w * y);
-		}
+		mlx_put_image_to_window(mlx, window, wall, w * x, 0);
+		mlx_put_image_to_window(mlx, window, wall, w * x, w * 9);
 	}
+	for (int y = 1; y < 9; y++)
+	{
+		mlx_put_image_to_window(mlx, window, wall, 0, w * y);
+		mlx_put_image_to_window(mlx, window, wall, w * 9, w * y);
+	}
+	for (int x = 1; x < 9; x++)
+	{
+		for (int y = 1; y < 9; y++)
+			mlx_put_image_to_window(mlx, window, empty, w * x, w * y);
+	}
+	mlx_put_image_to_window(mlx, window, exit, w * 8, w);
+	
 
 	return (mlx_loop(mlx));
 }
