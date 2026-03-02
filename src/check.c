c345becadd45d5de9bd6f0c49c18cc2c7afe3297
Eren <eren.g94@gmail.com>
2026-03-02 06:24:40 +0100

    fix left and right boundary check

--- src/check.c
+++ src/check.c
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 00:58:39 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/03/02 06:19:31 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/03/02 06:24:03 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,8 @@ bool	check_row(t_game *game, char *row, int y)
 {
 	int i;
 
+	if (row[0] != '1' || row[game->map_size[0] - 1] != '1')
+		return (game->error = MAP_INVALID_BOUNDARY, false);
 	if (y == 0 || y == game->map_size[1] - 1)
 	{
 		while (*row)
@@ -54,8 +56,6 @@ bool	check_row(t_game *game, char *row, int y)
 	i = 0;
 	while (row[i])
 	{
-		if ((i == 0 || i == game->map_size[0]) && row[i] != '1')
-			return (game->error = MAP_INVALID_BOUNDARY, false);
 		if (i > game->map_size[0] + 1)
 			return (game->error = MAP_INVALID_ROW_LENGTH, false);
 		if (!ft_strchr("10ECP<>XWLK\n", row[i++]))
